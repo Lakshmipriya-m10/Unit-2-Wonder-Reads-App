3f866d5f5fc15fbd8622cf215efd530be37ab27b
Lakshmi Priya <lakshmipriyaark@gmail.com>
2026-09-04 16:04:58 -0500

    git commit -m "Added Story entity"

---
+++ src/test/java/org/example/wonderreadsapi/WonderreadsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wonderreadsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderreadsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
